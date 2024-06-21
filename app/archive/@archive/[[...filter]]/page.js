ba117b8340886ce54ad4de44b330af844f4289c1
braverokmc79 <braverokmc79@gmail.com>
2024-06-21 19:43:22 +0900

    143.Catch-All 폴백 및 여러경로 세그먼트 처리하기

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,26 +1,58 @@
 import NewsList from "@/components/news-list";
-import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
+import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "@/lib/news";
 import Link from "next/link";
 import React from "react";
 
 const FilteredNewsPage = ({ params }) => {
   const filter = params.filter;
-  const links = getAvailableNewsYears();
-  const news = getNewsForYear(filter[0]);
 
+  const selectedYear=filter?.[0];
+  const selectedMonth=filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears();
+  if(selectedYear&& !selectedMonth){
+    news=getNewsForYear(selectedYear);
+    links=getAvailableNewsMonths(selectedYear);
+  }
+
+
+  if(selectedYear&&selectedMonth){
+    news=getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links=[];
+  }
+
+  let newContent=<p>No news found for the selected preriod.</p>
+
+  if(news && news.length>0){
+    newContent=<NewsList news={news}   />
+  }
+  
   return (
     <header id="archive-header">
       <nav>
         <ul>
-          {links.map((link) => (
-            <li key={link}>
-              <Link href={`/archive/${link}`}>{link}</Link>
-            </li>
-          ))}          
+        
+          {links.map((link) => {
+            
+            let href= selectedYear?
+            `/archive/${selectedYear}/${link}` :
+             `/archive/${link}` ;             
+
+            return (
+              <li key={link}>
+                <Link href={href}>{link}</Link>
+              </li>
+            )
+
+
+          })}          
         </ul>
       </nav>
       
-      <NewsList news={news}   />
+     {newContent}
+
+
     </header>
   );
 };
