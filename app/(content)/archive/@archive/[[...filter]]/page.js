e0dcb93976b652ff58fdaa7897d5533f3de9e569
braverokmc79 <braverokmc79@gmail.com>
2024-06-22 14:58:13 +0900

    update : 163.서스펜스가 있는 세분화된 데이터 가져오기

--- app/(content)/archive/@archive/[[...filter]]/page.js
+++ app/(content)/archive/@archive/[[...filter]]/page.js
@@ -1,68 +1,94 @@
 import NewsList from "@/components/news-list";
-import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "@/lib/newsDB";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/lib/newsDB";
 import Link from "next/link";
-import React from "react";
+import React, { Suspense } from "react";
 
-const FilteredNewsPage = async ({ params }) => {
-  const filter = params.filter;
+//헤더
+async function FilterHeader({ year, month }) {
+  const availableYears = await getAvailableNewsYears();
+  let links = availableYears;
 
-  const selectedYear=filter?.[0];
-  const selectedMonth=filter?.[1];
+  const availableMonth = getAvailableNewsMonths(year);
 
-  let news;
-  let links = await getAvailableNewsYears();
-  if(selectedYear&& !selectedMonth){
-    news= await getNewsForYear(selectedYear);
-    links= getAvailableNewsMonths(selectedYear);
+  if (year && !month) {
+    links = availableMonth;
   }
 
-
-  if(selectedYear&&selectedMonth){
-    news=await getNewsForYearAndMonth(selectedYear, selectedMonth);
-    links=[];
+  if (year && month) {
+    links = [];
   }
 
-  let newContent=<p>No news found for the selected preriod.</p>
-
-  if(news && news.length>0){
-    newContent=<NewsList news={news}   />
-  }
-
-  const availableYears=await getAvailableNewsYears();
   
-  if(
-      (selectedYear && !availableYears.includes(selectedYear) )
-      ||(selectedMonth && !getAvailableNewsMonths(selectedYear).includes(selectedMonth))){
+  if (
+    (year && !availableYears.includes(year)) ||
+    (month && !availableMonth.includes(month))
+  ) {
     throw new Error("Invalid filter.");
   }
-  
 
   return (
     <header id="archive-header">
       <nav>
         <ul>
-        
           {links.map((link) => {
-            
-            let href= selectedYear?
-            `/archive/${selectedYear}/${link}` :
-             `/archive/${link}` ;             
+            let href = year
+              ? `/archive/${year}/${link}`
+              : `/archive/${link}`;
 
             return (
               <li key={link}>
                 <Link href={href}>{link}</Link>
               </li>
-            )
-
-
-          })}          
+            );
+          })}
         </ul>
       </nav>
-      
-     {newContent}
-
-
     </header>
+  );
+}
+
+
+//필터 뉴스
+async function FilteredNews({ year, month }) {
+  let news;
+
+  if (year && !month) {
+    news = await getNewsForYear(year);
+  } else if (year && month) {
+    news = await getNewsForYearAndMonth(year, month);
+  }
+
+  let newContent = <p>No news found for the selected preriod.</p>;
+
+  if (news && news.length > 0) {
+    newContent = <NewsList news={news} />;
+  }
+
+  return newContent;
+}
+
+
+//본체
+const FilteredNewsPage = async ({ params }) => {
+  const filter = params.filter;
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  return (
+    <>
+      {/* <Suspense fallback={<p> 😶‍🌫️😶‍🌫️😶‍🌫️😶로딩 ...</p>}>
+        <FilterHeader year={selectedYear} month={selectedMonth} />
+      </Suspense> */}
+      <Suspense fallback={<p>🎈🎈🎈 fallback 로딩 news...</p>}>   
+        <FilterHeader year={selectedYear} month={selectedMonth} />    
+        <FilteredNews year={selectedYear} month={selectedMonth} />
+      </Suspense>
+    </>
   );
 };
 
---
+++ app/(content)/archive/@archive/loading.js
@@ -0,0 +1,3 @@
+export default function ArchiveLoading(){
+    return <p> archive 😊로딩중....</p>;
+}
